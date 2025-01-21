763449231fdbb9b9a133aa6a2f07970688723143
Noah Netz <noahjnetz@gmail.com>
2025-01-20 17:29:36 -0700

    Interrupt LED at 1 second

--- app/main.asm
+++ app/main.asm
@@ -86,10 +86,13 @@ SetupP6     bic.b   #BIT6,&P6OUT            ; Clear P6.6 output
 
 Timer_B0    bis.w   #TBCLR,&TB0CTL          ; Clear timer
             bis.w   #TBSSEL__SMCLK,&TB0CTL  ; Select SMCLK as timer source
-            bis.w   #MC__CONTINUOUS,&TB0CTL ; Continuous counting
-            bis.w   #ID__8,&TB0CTL
-            bis.w   #TBIE,&TB0CTL
-            bic.w   #TBIFG,&TB0CTL
+            bis.w   #MC__UP,&TB0CTL         ; UP counting
+            bis.w   #ID__4,&TB0CTL          ; Div-by-4
+            mov.w   #111,&TB0EX0              ; Divide by 8
+
+            mov.w   #32150,&TB0CCR0
+            bis.w   #CCIE,&TB0CCTL0
+            bic.w   #CCIFG,&TB0CCTL0
             bis.w   #GIE,SR
 
 Mainloop    xor.b   #BIT0,&P1OUT            ; Toggle P1.0 every 1s
@@ -113,7 +116,7 @@ Inner_loop  dec.w   R15
 ;------------------------------------------------------------------------------
 ISR_TB0_Overflow:
             xor.b   #BIT6,&P6OUT
-            bic.w   #TBIFG,&TB0CTL
+            bic.w   #CCIFG,&TB0CCTL0
             reti
             
 ;------------------------------------------------------------------------------
@@ -122,6 +125,6 @@ ISR_TB0_Overflow:
             .sect   RESET_VECTOR            ; MSP430 RESET Vector
             .short  RESET                   ;
 
-            .sect   ".int42"
+            .sect   ".int43"
             .short  ISR_TB0_Overflow
             .end
